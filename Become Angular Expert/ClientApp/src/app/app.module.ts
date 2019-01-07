aa150f7efa3361fc9ed7e35c9df35e5b3d66aacc
Michael Lunsford <michael.lunsford80@gmail.com>
2019-01-06 22:26:14 -0600

    Renamed CanDeactivateCreateIssue.ts

--- Become Angular Expert/ClientApp/src/app/app.module.ts
+++ Become Angular Expert/ClientApp/src/app/app.module.ts
@@ -12,7 +12,7 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { IssuesComponent } from './issues/issues.component';
 import { IssuesGridComponent } from './issues-grid/issues-grid.component';
 import { CreateIssueComponent } from './create-issue/create-issue.component';
-import { CanDeactivateCreateIssue } from './create-issue/CanDeactivateCreateIssue';
+import { CanDeactivateCreateIssue } from './create-issue/can-deactivate-create-issue';
 import { DialogComponent } from './dialog/dialog.component';
 import { DialogService } from './dialog/dialog.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
